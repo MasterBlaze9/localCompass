0db382becc437636c21b21cf413697565cf0b62b
MasterBlaze9 <ruinunoss@gmail.com>
2025-12-15 21:31:15 +0000

    BackendStart

---
+++ backend/localCompass/src/main/java/io/codeForAll/localCompass/LocalCompassApplication.java
@@ -0,0 +1,13 @@
+package io.codeForAll.localCompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalCompassApplication.class, args);
+	}
+
+}
---
+++ backend/localCompass/src/test/java/io/codeForAll/localCompass/LocalCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codeForAll.localCompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalCompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

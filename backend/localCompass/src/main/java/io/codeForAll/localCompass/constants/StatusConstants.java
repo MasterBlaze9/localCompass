226b6a9bf327a89c9e39a5793375883b015501bf
MasterBlaze9 <ruinunoss@gmail.com>
2025-12-18 23:37:50 +0000

    Final deploy

---
+++ backend/localCompass/src/main/java/io/codeForAll/localCompass/constants/StatusConstants.java
@@ -0,0 +1,11 @@
+package io.codeForAll.localCompass.constants;
+
+import io.codeForAll.localCompass.entites.enums.PostStatus;
+import io.codeForAll.localCompass.entites.enums.ReportStatus;
+import io.codeForAll.localCompass.entites.enums.EventStatus;
+
+public class StatusConstants {
+    public static final PostStatus DEFAULT_POST_STATUS = PostStatus.OPEN;
+    public static final ReportStatus DEFAULT_REPORT_STATUS = ReportStatus.OPEN;
+    public static final EventStatus DEFAULT_EVENT_STATUS = EventStatus.SCHEDULED;
+}
